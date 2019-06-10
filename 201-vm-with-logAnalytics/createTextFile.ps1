49061a62cfe58a7caf7fdc437d164fa40e03654f
Saadman Sayed <saadmansayed@gmail.com>
2019-06-10 16:39:52 +1000

    Add files via upload

---
+++ 201-vm-with-logAnalytics/createTextFile.ps1
@@ -0,0 +1,28 @@
+﻿configuration createTextFile
+{
+   param
+    (
+        [Parameter(Mandatory)]
+        [string]$vmTagValue
+    )
+    Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+    
+    Node localhost{
+
+    
+        File CreateFile {
+            DestinationPath = 'C:\Windows\Temp\sampledata.txt'
+            Ensure = "Present"
+            Contents = $vmTagValue
+        }
+        Log AfterDirectoryCopy
+        {
+            # The message below gets written to the Microsoft-Windows-Desired State Configuration/Analytic log
+            Message = "Finished running the file resource with ID CreateFile"
+            DependsOn = "[File]CreateFile" # This means run "CreateFile" first.
+        }
+
+
+    
+    }
+}
